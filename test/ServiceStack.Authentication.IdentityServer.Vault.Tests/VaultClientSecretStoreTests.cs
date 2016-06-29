2e2150c306a15d46b6ae634693c2220de8ff1038
stuartbfs <stuart@badfeelingstudios.com>
2016-06-29 09:47:43 +0100

    Namespace of the test project wasn't consistent with the assembly it is testing

--- test/ServiceStack.Authentication.IdentityServer.Vault.Tests/VaultClientSecretStoreTests.cs
+++ test/ServiceStack.Authentication.IdentityServer.Vault.Tests/VaultClientSecretStoreTests.cs
@@ -1,10 +1,10 @@
 ﻿// This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-namespace ServiceStack.Auth.IdentityServer.Vault.Tests
+namespace ServiceStack.Authentication.IdentityServer.Vault.Tests
 {
     using System;
-    using Authentication.IdentityServer.Vault;
+    using Vault;
     using Configuration;
     using FakeItEasy;
     using FluentAssertions;

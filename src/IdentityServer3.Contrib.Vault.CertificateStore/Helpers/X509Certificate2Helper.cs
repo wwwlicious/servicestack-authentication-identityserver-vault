32faea6d0411883c856ae5f14d18d79363480e6a
stuartbfs <stuart@badfeelingstudios.com>
2016-12-13 12:08:29 +0000

    Bug fix with location where certificates are stored

--- src/IdentityServer3.Contrib.Vault.CertificateStore/Helpers/X509Certificate2Helper.cs
+++ src/IdentityServer3.Contrib.Vault.CertificateStore/Helpers/X509Certificate2Helper.cs
@@ -6,29 +6,20 @@ namespace IdentityServer3.Contrib.Vault.CertificateStore.Helpers
     using System;
     using System.IO;
     using System.Security.Cryptography.X509Certificates;
-    using Core.Helpers;
     using IdentityServer3.Core.Logging;
     using Interfaces;
-    using Options;
     using Org.BouncyCastle.Security;
 
     public class X509Certificate2Helper : IX509Certificate2Helper
     {
         private static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
 
-        private readonly VaultCertificateStoreOptions options;
-
-        public X509Certificate2Helper(VaultCertificateStoreOptions options)
-        {
-            this.options = options.ThrowIfNull(nameof(options));
-        }
-
         /// <summary>Writes the Certificate to a temporary file then creates the Certificate from there</summary>
         /// <param name="certificateData">Vault Certificate Data</param>
         /// <returns>X509 Certificate 2</returns>
         public X509Certificate2 GetCertificate(byte[] certificateData)
         {
-            var file = Path.Combine(options.CertificateTempPath, "Idsvr" + Guid.NewGuid());
+            var file = Path.Combine(Path.GetTempPath(), "Idsvr" + Guid.NewGuid());
             X509Certificate2 certificate;
             try
             {
@@ -60,14 +51,15 @@ namespace IdentityServer3.Contrib.Vault.CertificateStore.Helpers
         /// <param name="certificate">X509 Certificate to write to disk</param>
         public void WriteCertificateToFile(string fileName, X509Certificate2 certificate)
         {
+            var file = Path.Combine(Path.GetTempPath(), fileName);
             try
             {
-                Logger.Debug($"Writing pfx certificate to {fileName}");
-                File.WriteAllBytes(fileName, certificate.Export(X509ContentType.Pfx, (string)null));
+                Logger.Debug($"Writing pfx certificate to {file}");
+                File.WriteAllBytes(file, certificate.Export(X509ContentType.Pfx, (string)null));
             }
             catch (Exception exception)
             {
-                Logger.Log(LogLevel.Error, () => $"Unable to write file {fileName}", exception);
+                Logger.Log(LogLevel.Error, () => $"Unable to write file {file}", exception);
                 throw;
             }
         }
@@ -79,9 +71,10 @@ namespace IdentityServer3.Contrib.Vault.CertificateStore.Helpers
         /// <returns>X509 Certificate from disk</returns>
         public X509Certificate2 LoadCertificate(string fileName)
         {
+            var file = Path.Combine(Path.GetTempPath(), fileName);
             try
             {
-                using (var stream = new FileStream(fileName, FileMode.Open))
+                using (var stream = new FileStream(file, FileMode.Open))
                 {
                     var bytes = new byte[stream.Length];
                     stream.Read(bytes, 0, bytes.Length);
@@ -95,12 +88,12 @@ namespace IdentityServer3.Contrib.Vault.CertificateStore.Helpers
             }
             catch (Exception exception)
             {
-                Logger.Log(LogLevel.Error, () => $"Unable to load certificate from file {fileName}", exception);
+                Logger.Log(LogLevel.Error, () => $"Unable to load certificate from file {file}", exception);
                 throw;
             }
             finally
             {
-                File.Delete(fileName);
+                File.Delete(file);
             }
         }
     }
